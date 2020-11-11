68344fb9d3fc9106cdf54403b26ddbaf53db5daf
puzzledpublic <puzzledpublic@gmail.com>
2020-11-11 23:57:53 +0900

    initial commit

---
+++ src/main/java/com/example/SpringbookCopy/SpringbookCopyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbookCopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbookCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringbookCopy/SpringbookCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbookCopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbookCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
